b87ec16edd6cb82d043c7c4cc52c03f3d9b3193b
jgoerner <joshua.goerner@gmail.com>
2019-02-07 13:33:58 +0100

    Include control task 1.5

---
+++ statistics/src/1-15-bic.py
@@ -0,0 +1,15 @@
+# Imports
+from sklearn.datasets import load_boston
+from sklearn.linear_model import LassoLarsIC
+
+# Prepare the data
+boston = load_boston()
+X = boston.data
+y = boston.target
+
+# Perform the Regression
+lr = LassoLarsIC(criterion="bic")
+lr.fit(X, y)
+
+# Evalutae
+print(lr.alpha_)
